5451a2e1e0f12d63a127120b72fa4149165d8c01
BenjaminDeJager <61642215+BenjaminDeJager@users.noreply.github.com>
2021-02-06 19:57:32 -0800

    re-pulling!

--- HUD.js
+++ HUD.js
@@ -2,7 +2,7 @@
 toggle between hiding and showing the dropdown content */
 function menuFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
-}
+};
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
@@ -16,4 +16,48 @@ window.onclick = function(event) {
       }
     }
   }
+};
+
+// Minimap for the game.
+class MiniMap {
+    constructor(game, x, y, w) {
+        Object.assign(this, { game, x, y, w });
+    };
+
+    updateMe() {
+
+    };
+
+    drawMe(ctx) {
+        // for (var i = 0; i < this.game.entities.length; i++) {
+        //    this.game.entities[i].drawMinimap(ctx, this.x, this.y);
+        // }
+
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.x, this.y, this.w, 192);
+
+    };
+
+};
+
+// Rest of the UI to hold the game menu features.
+class UI {
+    constructor(game, x, y, w) {
+        Object.assign(this, { game, x, y, w });
+    };
+
+    updateMe() {
+
+    };
+
+    drawMe(ctx) {
+        ctx.fillStyle = "SaddleBrown";
+        ctx.fillRect(this.x, this.y, this.w, 576);
+        ctx.font = params.TILE_W_H/4 + 'px "Playfair Display SC"';
+        ctx.fillStyle = "White";
+        ctx.fillText("MiniMap", this.x + 88, 568);
+        ctx.strokeStyle = "White"
+        ctx.strokeRect(this.x + 84, 552, 76, 22);
+    }
+
 }
